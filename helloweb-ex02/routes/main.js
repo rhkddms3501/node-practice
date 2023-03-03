b6f07f1b04dd473b61386e26cf0aab13a7e6af76
rhkddms3501 <rhkddms3501@naver.com>
2023-03-03 16:41:24 +0900

    node, node express

---
+++ helloweb-ex02/routes/main.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var router = express.Router();
+
+router.route('').get(function(req, res){
+    res.render('main/index');
+});
+
+
+
+
+
+module.exports = router;
